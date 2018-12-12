ae2c01d4461b5f840910c7e9d96f770e93fdb8e9
Sam <samuelhawley1992@gmail.com>
2018-12-13 00:04:54 +0800

    Add files via upload
    Episode 4 Completed

--- mario-js-game-master/js/main.js
+++ mario-js-game-master/js/main.js
@@ -7,8 +7,6 @@ import {createBackgroundLayer, createSpriteLayer} from './layers.js';
 
 import Keyboard from './KeyboardStates.js'
 
-
-
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
@@ -25,7 +23,6 @@ Promise.all([
 
     const gravity = 2000;
     mario.pos.set(64, 180);
-    mario.vel.set(200, -600);
 
     const SPACE = 32;
     const input = new Keyboard();
